6578d94a669206ba32bd43a5c5c93ae9e69aec6a
Thomas Keitel <tom@undpaul.de>
2016-06-26 22:54:01 +0200

    Add base class for drubo-enabled RoboFile console commands configuration classes

---
+++ src/Robo/Tasks.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drubo\Robo;
+
+use Robo\Tasks as RoboTasks;
+
+/**
+ * Base class for drubo-enabled RoboFile console commands configuration classes.
+ */
+abstract class Tasks extends RoboTasks {
+
+  /**
+   * Install Drupal site.
+   */
+  public function siteInstall() {
+    // TODO Implement Tasks::siteInstall().
+  }
+
+  /**
+   * Update Drupal site.
+   */
+  public function siteUpdate() {
+    // TODO Implement Tasks::siteUpdate().
+  }
+
+}
