942e9fa8d34d27b379b7c3d33344c5cc77164243
Thomas Keitel <tom@undpaul.de>
2017-02-14 21:01:56 +0100

    Rework Drupal reinstallation task to use no chain

--- src/Robo/Task/Drupal/Site/Reinstall.php
+++ src/Robo/Task/Drupal/Site/Reinstall.php
@@ -7,31 +7,21 @@ use Drubo\Robo\Task\DrupalConsole\ExecChain;
 /**
  * Robo task: Reinstall a Drupal site.
  */
-class Reinstall extends ExecChain {
+class Reinstall extends Install {
 
   /**
    * {@inheritdoc}
    */
-  protected function chainFile($packageDirectory) {
-    return $packageDirectory . '/.drupalconsole/chain/site/reinstall.yml';
-  }
+  protected function options() {
+    $options = parent::options();
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function chainFilePlaceholderValues() {
-    $config = $this->environmentConfig();
+    // Force installation.
+    $options['force'] = NULL;
 
-    return [
-      'account_mail' => $config->get('drupal.account.mail'),
-      'account_name' => $config->get('drupal.account.name'),
-      'account_pass' => $config->get('drupal.account.pass'),
-      'database' => 'default',
-      'langcode' => $config->get('drupal.site.language'),
-      'profile' => $config->get('drupal.site.profile'),
-      'site_mail' => $config->get('drupal.site.mail'),
-      'site_name' => $config->get('drupal.site.name'),
-    ];
+    // No interaction.
+    $options['no-interaction'] = NULL;
+
+    return $options;
   }
 
   /**
